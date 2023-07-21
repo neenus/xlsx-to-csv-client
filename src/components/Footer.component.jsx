50ef7bcfa07a1a78a71521d580acf58931a27bd3
Neenus Gabriel <hello@neenus.com>
2023-07-21 00:23:10 +0000

    feat(Footer): moved footer code into a component

---
+++ src/components/Footer.component.jsx
@@ -0,0 +1,24 @@
+import {
+  Box,
+  Link,
+  Typography,
+} from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <Box mt={18}>
+      <footer className="footer">
+        <Typography variant="body2" color="textSecondary" align="center">
+          {"Copyright © "}
+          <Link color="inherit" href="https://nraccounting.ca" target="_blank">
+            NR Accounting & Business Advisors Inc.
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </footer>
+    </Box>
+  );
+}
+
+export default Footer;
