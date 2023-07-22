27eb285285b74636da822341fd6b4a7fb029dd48
Neenus Gabriel <hello@neenus.com>
2023-07-22 16:20:16 +0000

    feat(App.tsx, index.tsx): update app and index to tsx and update code to work with react 18.x

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  createTheme,
+  ThemeProvider,
+  StyledEngineProvider
+} from "@mui/material/styles";
+import "./index.css";
+import App from "./App.tsx";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Playfair Display', serif"
+  }
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
