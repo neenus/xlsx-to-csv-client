862b351f7b4ca47c9a05cacdfa50cd4e6e754d0e
Neenus Gabriel <hello@neenus.com>
2023-07-24 05:10:16 +0000

    feat(App.tsx): update App.tsx to include new Header and add routes to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/homepage/HomePage";
 import About from "./pages/aboutpage/AboutPage";
+import Contractors from "./pages/contractors/ContractorsPage";
+import Services from "./pages/services/ServicesPage";
+import Header from "./components/Header.component";
 import Footer from "./components/Footer.component";
 import { ToastContainer } from "react-toastify";
 
@@ -10,6 +13,7 @@ const App = () => {
   return (
     <Router>
       <ToastContainer />
+      <Header />
       <div>
         <Switch>
           <Route exact path="/">
@@ -17,6 +21,12 @@ const App = () => {
           </Route>
           <Route exact path="/about">
             <About />
+          </Route>
+          <Route exact path="/contractors">
+            <Contractors />
+          </Route>
+          <Route exact path="/services">
+            <Services />
           </Route>
         </Switch>
       </div>
