39c93882934ae1e80358713e2b1bfcb8f35d018f
Neenus Gabriel <hello@neenus.com>
2024-03-02 15:47:14 +0000

    feat(AboutPage): update about page to she client and server version

--- src/pages/aboutpage/AboutPage.tsx
+++ src/pages/aboutpage/AboutPage.tsx
@@ -1,18 +1,59 @@
-import { Container, Link, Typography } from "@mui/material";
+import { Container, Typography, Box, Divider } from "@mui/material";
+import { Link } from "react-router-dom";
 import useTitle from "../../hooks/useTitle";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const AboutPage = () => {
+  const [serverVersion, setServerVersion] = useState<string>("");
   useTitle("About Excel to CSV Converter");
+
+  useEffect(() => {
+    const getServerVersion = async () => {
+      const apiUrl: string =
+        import.meta.env.MODE !== "production"
+          ? import.meta.env.VITE_API_BASE_URL
+          : import.meta.env.VITE_API_BASE_URL_PROD;
+      try {
+        const response = await axios.get(`${apiUrl}/api/v1/version`);
+        const serverVersion = response.data.version;
+        setServerVersion(serverVersion);
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    };
+
+    getServerVersion();
+  }, []);
+
   return (
     <Container maxWidth="lg">
-      <Typography variant="h4" component="h4" gutterBottom>
-        <Link href="/" color="inherit">
-          XLSX to CSV App
-        </Link>
+      <Typography
+        variant="h6"
+        component={Link}
+        to="/"
+        gutterBottom
+        sx={{
+          textDecoration: "none",
+          color: "inherit",
+          "&:hover": {
+            color: "primary.main"
+          }
+        }}
+      >
+        XLSX to CSV App
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        Build {import.meta.env.VITE_VERSION}
-      </Typography>
+
+      <Divider sx={{ marginBottom: 4, marginTop: 2 }} />
+
+      <Box>
+        <Typography variant="body1" gutterBottom>
+          Client version: {import.meta.env.VITE_VERSION}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Server version: {serverVersion}
+        </Typography>
+      </Box>
     </Container>
   );
 };
