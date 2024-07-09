75b089804a6553b939664bb155d1af2bf516eb1c
Neenus Gabriel <hello@neenus.com>
2024-07-09 16:15:10 +0000

    feat(apiservice.ts): add apiService to make auth api calls

---
+++ src/api/apiService.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+interface Credentials {
+  email: string;
+  password: string;
+};
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const baseURL: string =
+  import.meta.env.MODE !== "production"
+    ? `${import.meta.env.VITE_API_BASE_URL}/api/v1`
+    : import.meta.env.VITE_API_BASE_URL_PROD;
+
+const apiClient = axios.create({
+  baseURL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export const login = async (credentials: Credentials): Promise<User> => {
+  const response = await apiClient.post<User>("/auth/login", credentials);
+  return response.data;
+};
+
+export const logout = async (): Promise<void> => {
+  await apiClient.get("/auth/logout");
+};
